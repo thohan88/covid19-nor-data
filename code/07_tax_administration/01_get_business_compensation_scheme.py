a4666dfe0122c5551b40ef2424bec01b73631533
frefrik <fredrik@haarstad.no>
2020-07-03 01:33:04 +0200

    Map district and industry columns

--- code/07_tax_administration/01_get_business_compensation_scheme.py
+++ code/07_tax_administration/01_get_business_compensation_scheme.py
@@ -13,7 +13,7 @@ columns = ['Tildelingsdato','Organisasjonsnavn','Organisasjonsnummer','Naeringsk
            'OmsetningJanuarFebruar2019','OmsetningSoektPeriode','OmsetningTilsvarendePeriode2019',
            'Justeringsfaktor','Fylke']
 
-mapping = {'Tildelingsdato': 'grant_date',
+mapping = {'Tildelingsdato': 'date',
            'Organisasjonsnavn': 'company_name',
            'Organisasjonsnummer': 'organisation_number',
            'Naeringskode': 'industry_code',
@@ -25,15 +25,29 @@ mapping = {'Tildelingsdato': 'grant_date',
            'OmsetningSoektPeriode': 'actual_revenue_for_the_period',
            'OmsetningTilsvarendePeriode2019': 'turnover_corresponding_period_2019',
            'Justeringsfaktor': 'adjustment_factor',
-           'Fylke': 'district'}
+           'Fylke': 'fylke_name'}
 
-df = pd.read_csv(data, delimiter=';')
+df = pd.read_csv(data, delimiter=';', dtype={'Naeringskode': str})
+df = df[columns].rename(columns=mapping).sort_values(by='date', ascending=False)
 
-df = df[columns].rename(columns=mapping).sort_values(by='grant_date', ascending=False)
-
-df['grant_date'] = pd.to_datetime(df.grant_date, format='%Y-%m-%dT%H.%M.%SZ')
+df['date'] = pd.to_datetime(df.date, format='%Y-%m-%dT%H.%M.%SZ')
 df['period_month'] = pd.to_datetime(df.period_month, format='%B %Y')
-df['industry_code'] = df['industry_code'].round(3).astype(str)
+df['fylke_name'] = df['fylke_name'].fillna('Ukjent Fylke')
+
+# merge districts
+districts = pd.read_csv('data/00_lookup_tables_and_maps/01_lookup_tables/municipalities.csv', usecols=['fylke_name', 'fylke_no'], dtype=str).drop_duplicates()
+df = pd.merge(df, districts, how='left', on='fylke_name')
+
+# merge classifications
+classification = pd.read_csv('data/00_lookup_tables_and_maps/01_lookup_tables/standard_industrial_classification.csv', usecols=['code', 'name'], delimiter=';', encoding='ISO-8859-1', dtype=str)
+classification = classification[['code', 'name']].rename(columns={'code': 'industry_code', 'name': 'industry_name'})
+df = pd.merge(df, classification, how='left', on='industry_code')
+
+# reorder columns
+cols = list(df)
+cols.insert(12, cols.pop(cols.index('fylke_no')))
+cols.insert(13, cols.pop(cols.index('industry_code')))
+df = df[cols]
 
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
